45f1e335745bf69457549274718e8bd8abd102e2
Tristertian <tristertian@qq.com>
2026-01-06 00:08:25 +0800

    frist commit

---
+++ build/CMakeFiles/book_management.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/main.cpp" "CMakeFiles/book_management.dir/main.cpp.obj" "gcc" "CMakeFiles/book_management.dir/main.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/Book.cpp" "CMakeFiles/book_management.dir/src/Book.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/Book.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/BookManager.cpp" "CMakeFiles/book_management.dir/src/BookManager.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/BookManager.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/FileOperations.cpp" "CMakeFiles/book_management.dir/src/FileOperations.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/FileOperations.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/MainWindow.cpp" "CMakeFiles/book_management.dir/src/MainWindow.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/MainWindow.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/Sales.cpp" "CMakeFiles/book_management.dir/src/Sales.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/Sales.cpp.obj.d"
+  "C:/Users/86183/Desktop/CurriculumDesign/BMS/src/Statistics.cpp" "CMakeFiles/book_management.dir/src/Statistics.cpp.obj" "gcc" "CMakeFiles/book_management.dir/src/Statistics.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/book_management.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/book_management.dir/main.cpp.obj"
+  "CMakeFiles/book_management.dir/main.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/Book.cpp.obj"
+  "CMakeFiles/book_management.dir/src/Book.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/BookManager.cpp.obj"
+  "CMakeFiles/book_management.dir/src/BookManager.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/FileOperations.cpp.obj"
+  "CMakeFiles/book_management.dir/src/FileOperations.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/MainWindow.cpp.obj"
+  "CMakeFiles/book_management.dir/src/MainWindow.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/Sales.cpp.obj"
+  "CMakeFiles/book_management.dir/src/Sales.cpp.obj.d"
+  "CMakeFiles/book_management.dir/src/Statistics.cpp.obj"
+  "CMakeFiles/book_management.dir/src/Statistics.cpp.obj.d"
+  "book_management.exe"
+  "book_management.exe.manifest"
+  "book_management.pdb"
+  "libbook_management.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/book_management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
